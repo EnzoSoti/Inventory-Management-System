ea965d344dd8b9113650636e9a9db72375a003a9
Enzo <enzoparanedaniela00@gmail.com>
2025-06-26 19:30:31 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Inventory Management System</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBPHQJmNpr1OYflrqFAaj_isQdLJxuf8CQ",
+  authDomain: "inventory-management-sys-4ca5b.firebaseapp.com",
+  projectId: "inventory-management-sys-4ca5b",
+  storageBucket: "inventory-management-sys-4ca5b.firebasestorage.app",
+  messagingSenderId: "1059490894984",
+  appId: "1:1059490894984:web:532071184c4d52d7063b3d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
